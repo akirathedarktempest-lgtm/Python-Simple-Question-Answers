26d11c144839b547596b46d5ba7d8e3119f9bc93
purple(?) <akirathedarktempest@gmail.com>
2026-06-10 19:16:42 +0530

    Add questions and answers about Python

---
+++ main/ques-ans.py
@@ -0,0 +1,17 @@
+#This is the file which will have the questions of Python with answers
+#The format is simple, we will have a list, and then inside a list we will have another list
+#In the list inside the main list will be the questions and answers
+#And we will use indexing, 0 of the list will be a question, 1 will be the answer
+
+questions=[
+    ["What is Python?","Python is a high level programming language popular for its simplicity."],
+    ["What is a String?","String is the built-in text sequence data type used to represent textual data"],
+    ["What will be the answer of: \"10\"*2","Answer: \"1010\""],
+    ["How many data types do Python has?","There are 7 major"],
+    ["What do str means?","str is how the string data type is represented in code in Python."],
+    ["What do .lower() method of str do?","It changes all the alphabets and letters of a string in lower case. If the letter is in uppercase, it will turn to lower and the lower will remain lower."],
+    ["Are string mutable?","No, string are unmutable."],
+    ["What will be the output of\n\"12\"+2","An error, str can only + with str only."],
+    ["What will be the output of this:\nt=\"Hello\"\nt.upper()\nprint(t)","Hello"],
+    ["Find the syntax error:\nst.1=0","The . in st.1 causes the error. A variable can't have symbols except underscore _ "]
+]
